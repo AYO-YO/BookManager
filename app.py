ebd0c07c3b26befcb73a7b4c693c486f526ee6f1
ayo_yo <gxlandzcx@gmail.com>
2022-05-03 20:38:42 +0800

    图书管理

--- app.py
+++ app.py
@@ -3,6 +3,14 @@ import pymysql
 
 app = Flask(__name__)
 
+conn = pymysql.connect(
+    host='172.30.79.15',
+    user='book_manager',
+    password='fan123',
+    db='book_manager',
+    charset='utf8'
+)
+
 
 @app.route('/')
 def index():
@@ -33,10 +41,65 @@ def register():
 def main():
     match request.method:
         case 'GET':
-            return render_template('main.html')
-        case 'POST':
-            sql='select book._id,name,author,press,cls_name,active from book,book_cls where cls=book_cls._id'
-            
+            cur = conn.cursor()
+            sql = 'select book._id,name,author,press,cls_name from book,book_cls where cls=book_cls._id and active=1 order by _id'
+            cur.execute(sql)
+            content = cur.fetchall()
+            return render_template('main.html', content=content)
+
+
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
+    match request.method:
+        case 'GET':
+            cur = conn.cursor()
+            sql = 'select book._id,name,author,press,cls_name,active from book,book_cls where cls=book_cls._id order by _id'
+            cur.execute(sql)
+            content = cur.fetchall()
+            return render_template('admin.html', content=content)
+
+
+def excute_sql(sql, args=tuple()):
+    with conn.cursor() as cur:
+        if len(args) > 0:
+            cur.execute(sql, args)
+        else:
+            cur.execute(sql)
+        flag = cur.rowcount
+        conn.commit()
+    return flag
+
+
+@app.route('/control', methods=['GET', 'POST'])
+def control():
+    cmd = request.values.get('cmd')
+    book_id = request.values.get('book_id')
+    match cmd:
+        case 'down_store':
+            sql = 'update book set active = 0 where _id = %s'
+            flag = excute_sql(sql, (book_id,))
+            if flag:
+                return render_template('success.html', message='下架')
+        case 'up_store':
+            sql = 'update book set active = 1 where _id = %s'
+            flag = excute_sql(sql, (book_id,))
+            if flag:
+                return render_template('success.html', message='上架')
+        case 'del_book':
+            sql = 'delete from book where _id=%s'
+            flag = excute_sql(sql, (book_id,))
+            if flag:
+                return render_template('success.html', message='删除')
+
+
+@app.route('/add_book', methods=['GET', 'POST'])
+def add_book():
+    pass
+
+
+@app.route('/update_book', methods=['GET', 'POST'])
+def update_book():
+    pass
 
 
 if __name__ == '__main__':
