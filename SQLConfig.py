6df66dc7eaeefaa5ea1533febc3ac34f25215ef8
ayo_yo <gxlandzcx@gmail.com>
2022-05-22 18:50:46 +0800

    SQL数据库连接修正

--- SQLConfig.py
+++ SQLConfig.py
@@ -1,3 +1,3 @@
 import pymysql
 
-conn = pymysql.connect(host='114.116.105.214', user='book_manager', password='fan123', db='book_manager', charset='utf8')
+conn = pymysql.connect(host='114.116.105.214', user='book_manager', password='fan123', db='book_manager',port=3366, charset='utf8')
